e260b1bf5fe4ce90d3f352f358e3faeb306834e2
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-10-11 09:45:48 -0400

    added substance to api calls

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,19 @@
 const userLocationBtn = document.getElementById('userLocation')
 const stateSelectDDDiv = document.getElementById('stateDropDownDiv')
 const userZipForm = document.getElementById('zipSearch')
+const zipModalbtn = document.getElementById('zipSearchBtn')
+const cityModalbtn = document.getElementById('cityModalbtn')
+const citySearchInput = document.getElementById('citySearch')
 
 
 
-userZipForm.addEventListener('submit', function weatherByZip(e) {
-    e.preventDefault()
+// Search by Zip function
 
-    const userZipInput = document.getElementById('zipCode').value
+zipModalbtn.addEventListener('click', function weatherByZip(e) {
+    // e.preventDefault()
+    console.log('taco')
+    const userZipInput = document.getElementById('zipSearch').value
+
     var ZipRequest = `https://api.openweathermap.org/data/2.5/forecast?zip=${userZipInput}&appid=608f5d7e99b3bee2a797c9ab316ee2c6&units=imperial`
 
     console.log(userZipInput)
@@ -22,14 +28,13 @@ userZipForm.addEventListener('submit', function weatherByZip(e) {
         .then(function (data) {
             console.log(data);
 
-   
-            
-
-
             return
         })
 })
 
+
+
+// Search by User's current location function
 
 userLocationBtn.addEventListener ('click', function () {
 navigator.geolocation.getCurrentPosition((data)=>{weatherByCurrentLocation(data)})
@@ -68,6 +73,39 @@ function weatherByCurrentLocation(data) {
             return
         })
 }
+
+
+// Search by City & State function.
+// this will run with just city but will display first JSON package and may be incorrect city. State added for accuracy. 
+
+
+cityModalbtn.addEventListener('click', function weatherByCity(e) {
+    // e.preventDefault()
+    console.log('taco')
+    const userCityInput = document.getElementById('citySearch').value
+    const userStateInput = document.getElementById('stateSelect').value
+    
+
+    var CityRequest = `https://api.openweathermap.org/data/2.5/forecast?q=${userCityInput},${userStateInput}&appid=608f5d7e99b3bee2a797c9ab316ee2c6&units=imperial`
+
+    console.log(userCityInput)
+  
+
+
+    fetch(CityRequest)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            return
+        })
+})
+
+
+
+
 
 
 
